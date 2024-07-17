160204907e5b633b8ba755cf1ead3586ee1bb6c7
Katie Kermode <kermodekatie@gmail.com>
2024-07-17 08:34:45 +0100

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import {
   Link,
   Navigate,
 } from "react-router-dom";
+import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
@@ -14,6 +15,7 @@ function App() {
   return (
     <Router>
       <div>
+        <h1>Dashboard25</h1>
         <nav>
           <ul>
             <li>
